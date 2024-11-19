178c888adb39ec47218351494804fbb539173bed
Tommer <souirtommer@gmail.com>
2024-11-19 13:40:17 +0800

    Add test dbconnection

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/', function () {
-    return view('welcome');
+    try {
+        DB::connection()->getPdo();
+        return 'MySQL connection is working!';
+    } catch (\Exception $e) {
+        return 'Could not connect to the database. Please check your configuration. Error: ' . $e->getMessage();
+    }
 });
