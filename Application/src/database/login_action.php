aca29cea08a5d7cc5842b92c4835a78bc3dc16dd
Tommer <souirtommer@gmail.com>
2024-11-20 22:22:15 +0800

    Select key based on role

--- Application/src/database/login_action.php
+++ Application/src/database/login_action.php
@@ -3,7 +3,6 @@
 require_once '../connection/mysqli_conn.php';
 session_start();
 
-
 $aesKeys = [
     'Patient' => getenv('AES_KEY_PATIENT'),
     'Secretary' => getenv('AES_KEY_SECRETARY'),
@@ -14,36 +13,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = trim($_POST['username']);
     $password = trim($_POST['password']);
 
-    $aesKey = $aesKeys['Patient'];
-    $stmt = $conn->prepare("SELECT AccountID, AES_DECRYPT(Password, ?, IV, 'hkdf') AS DecryptedPassword, Role FROM Accounts WHERE Username = ?");
-    $stmt->bind_param("ss", $aesKey, $username);
+    $stmt = $conn->prepare("SELECT Role FROM Accounts WHERE Username = ?");
+    $stmt->bind_param("s", $username);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows > 0) {
-        $stmt->bind_result($accountId, $decryptedPassword, $role);
+        $stmt->bind_result($role);
         $stmt->fetch();
+        $stmt->close();
 
-        if (password_verify($password, $decryptedPassword)) {
-            $_SESSION['username'] = $username;
-            $_SESSION['accountId'] = $accountId;
-            $_SESSION['role'] = $role;
-            if ($role === 'Patient') {
-                header("Location: ../welcome.php");
-            } elseif ($role === 'secretary') {
-                header("Location: ../Żecretary.php");
-            } elseif ($role === 'LabStaff') {
-                header("Location: ../welcome.php");
+        $aesKey = $aesKeys[$role];
+
+        $stmt = $conn->prepare("SELECT AccountID, AES_DECRYPT(Password, ?, IV, 'hkdf') AS DecryptedPassword, Role FROM Accounts WHERE Username = ?");
+        $stmt->bind_param("ss", $aesKey, $username);
+        $stmt->execute();
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            $stmt->bind_result($accountId, $decryptedPassword, $role);
+            $stmt->fetch();
+
+            if (password_verify($password, $decryptedPassword)) {
+                $_SESSION['username'] = $username;
+                $_SESSION['accountId'] = $accountId;
+                $_SESSION['role'] = $role;
+                if ($role === 'Patient') {
+                    header("Location: ../welcome.php");
+                } elseif ($role === 'Secretary') {
+                    header("Location: ../secretary.php");
+                } elseif ($role === 'LabStaff') {
+                    header("Location: ../welcome.php");
+                }
+                exit();
+            } else {
+                header("Location: ../login.php?message=Invalid%20password");
             }
-            exit();
         } else {
             header("Location: ../login.php?message=Invalid%20password");
         }
+
+        $stmt->close();
     } else {
-        header("Location: ../login.php?message=Invalid%20password");
+        header("Location: ../login.php?message=Invalid%20username");
     }
 
-    $stmt->close();
     $conn->close();
 }
 ?>
