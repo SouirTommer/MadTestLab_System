94810f28c31b04e5665dd0ef84e0e89034a3a155
Tommer <souirtommer@gmail.com>
2024-11-22 23:32:12 +0800

    add role check in session

--- Application/src/auth.php
+++ Application/src/auth.php
@@ -4,8 +4,34 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 function check_login() {
-    if (!isset($_SESSION['username'])) {
+    if (!isset($_SESSION['username']) || !isset($_SESSION['accountId']) || !isset($_SESSION['role'])) {
         header("Location: login.php");
+        exit();
+    }
+}
+//例子
+// allow these role. (Secretary and LabStaff)
+// 
+// <?php
+// session_start();
+
+// require 'auth.php';
+// check_role(['Secretary', 'LabStaff']);
+//
+
+function check_role($roles) {
+    check_login();
+    if (!in_array($_SESSION['role'], (array)$roles)) {
+        $redirects = [
+            'Patient' => 'patient.php',
+            'Secretary' => 'secretary.php',
+            'LabStaff' => 'labstaff.php',
+            'default' => 'login.php'
+        ];
+
+        $role = $_SESSION['role'];
+        $redirect_url = isset($redirects[$role]) ? $redirects[$role] : $redirects['default'];
+        header("Location: $redirect_url");
         exit();
     }
 }
